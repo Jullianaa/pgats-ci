9acc7dbd72267e30e6136b1364665b3bd1e0af06
Julliana do Couto Almeida <julliana.coutoalmeida@gmail.com>
2026-06-21 16:56:43 -0300

    Create gestaoProdutos.test.js

---
+++ test/gestaoProdutos.test.js
@@ -0,0 +1,18 @@
+import {pegarNomeDoProduto} from '../src/gestaoProdutosJu.js';
+import assert from 'node:assert';
+
+describe('testes gestao produto', function(){
+    it('validar que o vetor de indice 2 retorna macBook', function(){
+        //Arrage: o que eu preciso para testar a função
+        const indice = 2;
+        const nomeEsperado = 'MacBook Pro M5';
+
+        //Act
+        const nomeRetornado = pegarNomeDoProduto(indice);
+
+        //Assert
+        assert.equal(nomeRetornado,nomeEsperado);
+    });
+});
+
+
