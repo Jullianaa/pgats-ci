b3efdca5e17ebcb38ad21656b1e3ca8757915f5f
Julliana do Couto Almeida <julliana.coutoalmeida@gmail.com>
2026-06-21 18:50:50 -0300

    Update gestaoProdutos.js

--- src/gestaoProdutos.js
+++ src/gestaoProdutos.js
@@ -1,22 +1,29 @@
-const produtos =[
+
+const produtos = [
     {
-        nome: 'iphone17',
-        preco: '12000'
+        nome: 'Trackinas',
+        preco: '1.49',
+        estoque: 50
     },
     {
-        nome: 'Macbook m2',
-        preco: '9000'
+        nome: 'ps4',
+        preco: '3999.99',
+        estooque: 25  
     },
     {
-        nome:'airpod',
-        preco: '4000'
+        nome: 'MacBook',
+        preco: '21490.00',
+        estoque: 5,
+        cores: ['cinza espacial','cinza']  
     }
 ];
 
-for (let i=0;i<produtos.length;i++){
-    console.log('-------------------')
-    console.log(produtos.at(i).nome)
-    console.log(produtos.at(i).preco)
-    
+//console.log(produtos[2].cores[1])
+//console.log(produtos[2].nome)
+produtos[2].nome = 'MacBook Pro M5';
+//console.log(produtos[2].nome)
+
+
+export function pegarNomeDoProduto(indice){
+    return produtos[indice].nome
 }
-console.log('-------------------')
